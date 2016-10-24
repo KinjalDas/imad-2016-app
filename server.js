a8b66254a382737574fc005a92231ca3da73415f
KinjalDas <kinjal.das86@gmail.com>
2016-10-24 15:27:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/chat', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'chat.html'));
+});
+
+app.get('/contact_us', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'contact_us.html'));
+});
+
+app.get('/members_area', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'members_area.html'));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`MY app listening on port ${port}!`);
