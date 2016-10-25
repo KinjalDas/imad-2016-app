5c41d8e3a21822cc0b4bb79e9a49ca6009035ad2
KinjalDas <kinjal.das86@gmail.com>
2016-10-25 18:39:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,27 +1,34 @@
 console.log('Loaded!');
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+document.getElementById("navMenu").innerHTML =
+  '<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">'+ 
+        '<div class="navbar-header">'+ 
+              '<a class="navbar-brand" href="/">MychatAPP!</a>'+ 
+        '</div>'+ 
+        '<div>'+ 
+            '<ul class="nav navbar-nav">'+ 
+                '<li><a href="/">HOME</a></li>'+ 
+                '<li><a href="chat" data-target=".bodyarea">CHAT</a></li>'+ 
+                '<li><a href="contact_us">CONTACT ME</a></li>'+
+                '<li class="dropdown">'+ 
+                    '<a href="#" class="dropdown-toggle" data-toggle="dropdown">'+ 
+                    'LOGIN/REGISTER <b class="caret"></b>'+
+                    '</a>'+ 
+                    '<ul class="dropdown-menu">'+ 
+                    '<li><a href="members_area">LOGIN</a></li>'+ 
+                    '<li><a href="members_area">REISTER</a></li>'+ 
+                    '<li class="divider"></li>'+ 
+                    '<li><a href="#">Separated link</a></li>'+ 
+                    '<li class="divider"></li>'+ 
+                    '<li><a href="#">One more separated link</a></li>'+ 
+                    '</ul>'+
+                '</li>'+ 
+                '<form class="navbar-right pull-right navbar-form" role="search">'+
+                    '<div class="form-group">'+
+                        '<input type="text" class="form-control" placeholder="Search">'+
+                    '</div>'+
+                    '<button type="submit" class="btn btn-default">Submit</button>'+
+                '</form>'+
+            '</ul>'+ 
+        '</div>'+
+    '</nav>'; 
